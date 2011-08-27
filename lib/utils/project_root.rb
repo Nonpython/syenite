c088c47d6bba4eb94ae0631a2bcb41f2cbf303f1
Hayes Williams <sasinestro@gmail.com>
2011-08-27 03:42:33 -0700

    Rename some utilities to be more semantic

--- lib/utils/project_root.rb
+++ lib/utils/project_root.rb
@@ -1,5 +1,5 @@
 module Syenite
-    def find_project_root
+    def project_root
         Dir.chdir '..' while not File.directory? '.syenite'
         return Dir.pwd
     end
--- lib/utils/syenite_dir.rb
+++ lib/utils/syenite_dir.rb
@@ -1,6 +1,6 @@
-require 'find_project_root.rb'
+require 'project_root.rb'
 module Syenite
-    def find_syenite_dir
+    def syenite_dir
         return File.join find_project_root, '.syenite'
     end
 end
